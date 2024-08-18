b77c9b8f66ea63a52d87ffc1a907e87af9d87140
clover0916 <youhuid05@gmail.com>
2024-08-19 02:13:41 +0900

    refactor: Update enum order in EAUploaderMeta.cs

--- Runtime/Components/EAUploaderMeta.cs
+++ Runtime/Components/EAUploaderMeta.cs
@@ -5,7 +5,7 @@ namespace EAUploader.Components
     [DisallowMultipleComponent]
     public class EAUploaderMeta : AvatarTagComponent
     {
-        public enum PrefabStatus { Pinned, Show, Hidden, Other }
+        public enum PrefabStatus { Show , Hidden, Pinned }
         public enum PrefabType { VRChat, VRM, Other }
         public enum PrefabGenre { Avatar, Cloth, Accessory, Other }
 
